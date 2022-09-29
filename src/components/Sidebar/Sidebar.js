86918dfe826bc8d121e0ea4e06f682e966c036c5
dev-mahfuz <mrsohag04@gmail.com>
2022-09-29 17:19:18 +0600

    break and user info done

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import user from '../../img/Mahfuz.jpg';
 
 const Sidebar = () => {
     return (
-        <div>
+        <div className='sidebar'>
             <div className='usercard'>
-
+                <img src={user} />
+                <p><b>Mahfuzar Rahman</b> <br /> Dhaka, Bangladesh</p>
+            </div>
+            <div className="userinfo">
+                <div><span>65</span> Kg<span>Weight</span></div>
+                <div><span>5.5</span><span>Height</span></div>
+                <div><span>23</span> Yrs<span>Age</span></div>
+            </div>
+            <h3>Add a break</h3>
+            <div className="addbreak">
+                <button>10s</button>
+                <button>20s</button>
+                <button>30s</button>
+                <button>40s</button>
+                <button>50s</button>
             </div>
         </div>
     );
